ac7c0c8bc0719b0a3be1910b52e26b35fa026883
hecheff <44626838+hecheff@users.noreply.github.com>
2020-10-21 22:22:54 +0900

    Updated version code for CSS files.

--- php/constants.php
+++ php/constants.php
@@ -1,6 +1,6 @@
 <?php
     // Common
-    define('CSS_VERSION', '0.022-20201015'); 
+    define('CSS_VERSION', '0.025-20201021'); 
     define('TIMESTAMP_NOW', date("Y-m-d H:i:s"));
 
     // Database & Domain details
