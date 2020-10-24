718dff96195f3ca7ac1a83dfc6f587531138448e
hecheff <44626838+hecheff@users.noreply.github.com>
2020-10-24 13:24:27 +0900

    Added Statcounter tracking code. Established language components.

---
+++ lang/en/common.php
@@ -0,0 +1,14 @@
+<?php
+    // English language data
+    // Section: Common (Site-wide)
+    
+    class Lang {
+        public function GetLanguageData() {
+
+            // Notices
+            $lang['notice_login_success']   = "";
+            $lang['notice_login_failed']    = "";
+
+            return $lang;
+        }
+    }
--- php/footer.php
+++ php/footer.php
@@ -1,6 +1,26 @@
 <div class="footer">
     
 </div>
+<!-- Default Statcounter code for HaroPlanet: PC Parts
+Checker https://pc-parts-price-checker.haroplanet.online/
+-->
+<script type="text/javascript">
+    var sc_project=12415917; 
+    var sc_invisible=1; 
+    var sc_security="f691ac72"; 
+    </script>
+    <script type="text/javascript"
+    src="https://www.statcounter.com/counter/counter.js"
+    async>
+</script>
+<noscript>
+    <div class="statcounter"><a title="Web Analytics"
+    href="https://statcounter.com/" target="_blank"><img
+    class="statcounter"
+    src="https://c.statcounter.com/12415917/0/f691ac72/1/"
+    alt="Web Analytics"></a></div>
+</noscript>
+<!-- End of Statcounter Code -->
 <?php  
     // Reset notifications at end of page load
     $_SESSION['notices'] = "";
